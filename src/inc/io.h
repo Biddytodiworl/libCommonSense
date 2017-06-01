6c5e1d2e30c6aa3267e72469a8c3b88317a20f84
Brendon Henry <biddytodiworl@outlook.com>
2017-06-01 18:11:29 -0500

    Added C++ support

--- src/inc/io.h
+++ src/inc/io.h
@@ -3,15 +3,24 @@
 
 #include <stdio.h>
 
-/* Implementation for bool in C89 */
+#ifndef __cplusplus
+
 typedef enum {
 	false,
 	true
-} bool;
+} bool;	/* Define bool if C++ is not being used */
+
+#else
+extern "C" {
+#endif
 
 /* Function prototypes */
 size_t GetFileSize(FILE *);	/* Get size of file */
 size_t GetFileNameSize(const char *);	/* Get size of named file */
 bool CopyFile(const char *, const char *);	/* Copy file to another file */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- src/inc/math.h
+++ src/inc/math.h
@@ -1,9 +1,17 @@
 #ifndef CMN_MATH_H
 #define CMN_MATH_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Function prototypes */
 double Square(const double);	/* Return the number's square */
 double Cube(const double);	/* Returns the number's cube */
 int Factorial(const int);	/* Returns number's factorial */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
